8911f61b71395ea2d42d7d1c996c4e5455196414
majany <majany94@gmail.com>
2019-08-25 20:36:49 +0200

    improved completion item trigger

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,8 @@ export async function activate(context: vscode.ExtensionContext) {
 	const i18nglob = "**/i18n.properties"; // TODO: make configurable
 	const i18nFileProperties  = new properties.PropertiesFile();
 
+	// const uris = await vscode.workspace.findFiles(i18nglob);
+
 	function addFile(uri: vscode.Uri){ 
 		i18nFileProperties.addFile(uri.fsPath); 
 	}
@@ -29,18 +31,41 @@ export async function activate(context: vscode.ExtensionContext) {
 		scheme: "file"
 	}, {
 		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
-			let range = document.getWordRangeAtPosition(position);
+			let range = document.getWordRangeAtPosition(position, /[A-Za-z0-9>_]+/);
 			let text = document.getText(range);
-			if(range && (text.startsWith("i18n") || text.startsWith("i18"))){
-					return i18nFileProperties.getKeys().map((key: string) => {
-						let item = new vscode.CompletionItem(`i18n>${key}`, vscode.CompletionItemKind.Field);
+
+			let prefix = "i18n>";
+			if(range && text.startsWith("i18n>")){
+				prefix = "";
+			}
+			
+			if(range && (text.startsWith("i18n>") || text.startsWith("i18n"))){
+					let textExists = false;
+					// show existing i18n properties
+					let i18nCompletionItems = i18nFileProperties.getKeys().map((key: string) => {
+						let item = new vscode.CompletionItem(prefix + key, vscode.CompletionItemKind.Field);
 						item.detail = i18nFileProperties.getLast(key);
+						textExists = textExists || (prefix === "" && key === text.slice(5));
 						return item;
 					});
+
+					// new i18n property
+					// if(prefix === "" && text.length > 7 && vscode.CompletionTriggerKind.Invoke === context.triggerKind && !textExists ){
+					// 	let placeholder = new vscode.CompletionItem(text.slice(5) , vscode.CompletionItemKind.Snippet);
+					// 	placeholder.detail = "Create i18n property";
+					// 	placeholder.command = {
+					// 		command: "vscode.openFolder",
+					// 		arguments: [uris[0]],
+					// 		title: ""
+					// 	};
+					// 	placeholder.sortText = "z" + placeholder.label;
+					// 	i18nCompletionItems.push(placeholder);
+					// }
+					return i18nCompletionItems;
 			}
 			return [];
 		}
-	}, "n", "8");
+	}, ">", "n");
 
 	context.subscriptions.push(itemprovider);
 	context.subscriptions.push(i18nFileWatcher);
